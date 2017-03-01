c46606e1a6ed11fbc2273916e1cc906d2d33f01f
dawnos <litang@gmail.com>
2017-03-01 22:04:16 +0800

    Change to control flow

--- dcgan.py
+++ dcgan.py
@@ -16,20 +16,20 @@ def lrelu(x, alpha=0.2, name="lrelu"):
     return tf.maximum(x, alpha * x, name=name)
 
 
-def generator(input_tensor, reuse=False):
-    with tf.variable_scope('generator', reuse=reuse):
+def generator(input_tensor, name='Generator', reuse=False):
+    with tf.variable_scope(name, reuse=reuse):
         net = layers.stack(input_tensor, layers.conv2d_transpose, [
-            [ngf*8, [4, 4], 2, 'VALID', 'NHWC', tf.nn.relu, layers.batch_norm, {'reuse':reuse, 'scope':'generator'}],
-            [ngf*4, [4, 4], 2,  'SAME', 'NHWC', tf.nn.relu, layers.batch_norm],
-            [ngf*2, [4, 4], 2,  'SAME', 'NHWC', tf.nn.relu, layers.batch_norm],
-            [ngf*1, [4, 4], 2,  'SAME', 'NHWC', tf.nn.relu, layers.batch_norm],
-            [   nc, [4, 4], 2,  'SAME', 'NHWC', tf.nn.tanh, layers.batch_norm]
-        ])
+            [ngf*8, [4, 4], 2, 'VALID', 'NHWC', tf.nn.relu],
+            [ngf*4, [4, 4], 2,  'SAME', 'NHWC', tf.nn.relu],
+            [ngf*2, [4, 4], 2,  'SAME', 'NHWC', tf.nn.relu],
+            [ngf*1, [4, 4], 2,  'SAME', 'NHWC', tf.nn.relu],
+            [   nc, [4, 4], 2,  'SAME', 'NHWC', tf.nn.tanh]
+        ], normalizer_fn=layers.batch_norm)
     return net
 
 
-def discriminator(input_tensor, reuse=False):
-    with tf.variable_scope('discriminator', reuse=reuse):
+def discriminator(input_tensor, name='Discriminator', reuse=False):
+    with tf.variable_scope(name, reuse=reuse):
         net = layers.stack(input_tensor, layers.conv2d, [
             [ndf*1, [4, 4], 2, 'SAME', None, 1, lrelu],
             [ndf*2, [4, 4], 2, 'SAME', None, 1, lrelu],
@@ -40,51 +40,55 @@ def discriminator(input_tensor, reuse=False):
     return net
 
 
-def binary_cross_entropy(op1, op2, name=None):
-    return tf.reduce_mean(op1 * tf.log(op2) + (1-op1)*tf.log(1-op2), name=name)
+def binary_cross_entropy(op1, op2, name='BinaryCrossEntropy'):
+    with tf.variable_scope(name):
+        return tf.reduce_mean(op1 * tf.log(op2) + (1-op1)*tf.log(1-op2))
 
 
 def main(_):
 
     batch_size = 64
 
-    noise = tf.random_uniform((batch_size, 1, 1, nc), -1, 1, name='noise')
-    pos_labels = tf.constant(1, shape=(batch_size, 1, 1, 1), dtype=tf.float32)
-    neg_labels = tf.constant(0, shape=(batch_size, 1, 1, 1), dtype=tf.float32)
-    # real_images = tf.placeholder(tf.float32, (None, 64, 64, 3))
-    real_images = create_celeba_pipeline('/mnt/DataBlock/CelebA/Img/img_align_celeba.tfrecords')
+    noise = tf.random_uniform((batch_size, 1, 1, nc), -1, 1, name='Noise')
+    pos_labels = tf.constant(1, shape=(batch_size, 1, 1, 1), dtype=tf.float32, name='Ones')
+    neg_labels = tf.constant(0, shape=(batch_size, 1, 1, 1), dtype=tf.float32, name='Zeros')
+    real_images = create_celeba_pipeline('/mnt/DataBlock/CelebA/Img/img_align_celeba.tfrecords', name='ImagePipeline')
+    state = tf.placeholder(tf.int32, name='State')
 
-    g_d_1_op = discriminator(generator(noise))                          # For training generator
-    g_d_2_op = discriminator(generator(noise, reuse=True), reuse=True)  # for training discriminator with fake data
-    d_op = discriminator(real_images, reuse=True)                       # for training discriminator with real data
+    g_op = generator(noise, name='Generator')
+    d_input_op = tf.cond(
+        tf.equal(state, tf.constant(0), 'State0'), lambda: real_images, lambda: g_op, name='DiscriminatorInputSwitch')
+    d_op = discriminator(d_input_op, name='Discriminator')
+    label_op = tf.cond(tf.equal(state, tf.constant(1), 'State1'), lambda: neg_labels, lambda: pos_labels, name='Label')
+    loss = binary_cross_entropy(d_op, label_op, name='loss')
 
-    gen_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='generator')
-    dis_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
+    gen_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Generator')
+    dis_var = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Discriminator')
 
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    g_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'Generator')
+    d_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'Discriminator')
 
-    g_d_1_loss = binary_cross_entropy(g_d_1_op, pos_labels, name='g_d_1_loss')
-    g_d_2_loss = binary_cross_entropy(g_d_2_op, neg_labels, name='g_d_2_loss')
-    d_loss = binary_cross_entropy(d_op, pos_labels, 'd_loss')
+    with tf.control_dependencies(g_update_ops):
+        g_train_op = tf.train.AdamOptimizer(0.0002, 0.5).minimize(loss, var_list=gen_var, name='GTrain')
 
-    g_train_op = tf.train.AdamOptimizer(0.0002, 0.5).minimize(g_d_1_loss, var_list=gen_var, name='g_train')
-    d_real_train_op = tf.train.AdamOptimizer(0.0002, 0.5).minimize(d_loss, var_list=dis_var, name='real_d_train')
-    d_fake_train_op = tf.train.AdamOptimizer(0.0002, 0.5).minimize(g_d_2_loss, var_list=dis_var, name='fake_d_train')
+    with tf.control_dependencies(d_update_ops):
+        d_train_op = tf.train.AdamOptimizer(0.0002, 0.5).minimize(loss, var_list=dis_var, name='DTrain')
 
     with tf.Session() as sess:
 
         # Save the graph
         writer = tf.summary.FileWriter(logdir=FLAGS.log_dir, graph=tf.get_default_graph())
 
+        exit()
         # 1. Training discriminator
-        # 1.1 Train with real(feed:image, update_variable:discriminator)
-        sess.run(d_real_train_op)
+        # 1.1 Train with real(feed:image, update_variable:discriminator)    --> step 0
+        sess.run(d_train_op, feed_dict={state:0})
 
-        # 1.2 Train with fake(feed:noise, update_variable:discriminator)
-        sess.run(d_fake_train_op)
+        # 1.2 Train with fake(feed:noise, update_variable:discriminator)    --> step 1
+        sess.run(d_train_op, feed_dict={state:1})
 
-        # 2. Training generator(feed:noise, update_variable:generator)
-        sess.run(g_train_op)
+        # 2. Training generator(feed:noise, update_variable:generator)      -->step 2
+        sess.run(g_train_op, feed_dict={state:2})
 
 
 if __name__ == '__main__':
